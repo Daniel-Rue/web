ec00185ea6d31dc05aaeb1a9147f4283b9b16231
Daniel-Rue <78970069+Daniel-Rue@users.noreply.github.com>
2023-05-25 14:20:35 +0300

    Delete arts_and_crafts directory

--- arts_and_crafts/js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('login').addEventListener('click', function() {
-    let loginForm = document.getElementById('loginForm');
-    loginForm.style.display = loginForm.style.display === 'none' ? 'block' : 'none';
-});
